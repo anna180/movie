ea1331a5534afd858fafcb5ef47cfe86be6662ec
anna180 <106851083+anna180@users.noreply.github.com>
2023-06-08 21:25:54 +0300

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                The website for watching TV series and movies is an excellent platform for those who enjoy streaming content online. It offers a wide variety of movies and TV shows from every genre imaginable. Whether it's action, horror, drama, comedy, or romance, the website has something for everyone. 
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
